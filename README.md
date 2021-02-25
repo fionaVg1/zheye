1c36580d50fdeaf7c22698fba1d45f0c891c9d54
caiwanxia <caiwanxia@dm-ai.cn>
2021-02-25 16:31:12 +0800

    1、项目结构调整 2、ColumnList组件加入

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # zheye
-vue3.0 模拟知乎做的一个实践项目
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <column-list :list="testData"></column-list>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: "test1的专栏",
+    description: "这个是test1的专栏，有一段非常有意思的简介，可以更新一下哦",
+    avatar: "https://24.media.tumblr.com/tumblr_krvv7wTHnB1qa9hjso1_1280.jpg"
+  },
+  {
+    id: 2,
+    title: "test2的专栏",
+    description: "这个是test2的专栏，有一段非常有意思的简介，可以更新一下哦",
+    avatar: "https://images.dog.ceo/breeds/mexicanhairless/n02113978_3318.jpg"
+  }
+];
+export default defineComponent({
+  name: "App",
+  components: {
+    ColumnList
+  },
+  setup() {
+    return {
+      testData
+    };
+  }
+});
+</script>
+
+<style lang="scss">
+</style>

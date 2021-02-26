95a2784a0310bfbf87e5fb4be0aee0a45a5340fd
caiwanxia <caiwanxia@dm-ai.cn>
2021-02-26 15:27:45 +0800

    GlobalHeader.vue 顶部导航栏组件加入

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <global-header :user="currentUser"></global-header>
     <column-list :list="testData"></column-list>
   </div>
 </template>
@@ -8,6 +9,11 @@
 import { defineComponent } from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
+import GlobalHeader, { UserProps } from "./components/GlobalHeader.vue";
+const currentUser: UserProps = {
+  isLogin: true,
+  name: "兔子小姐"
+};
 const testData: ColumnProps[] = [
   {
     id: 1,
@@ -20,16 +26,23 @@ const testData: ColumnProps[] = [
     title: "test2的专栏",
     description: "这个是test2的专栏，有一段非常有意思的简介，可以更新一下哦",
     avatar: "https://images.dog.ceo/breeds/mexicanhairless/n02113978_3318.jpg"
+  },
+  {
+    id: 3,
+    title: "test3的专栏",
+    description: "这个是test3的专栏，有一段非常有意思的简介，可以更新一下哦"
   }
 ];
 export default defineComponent({
   name: "App",
   components: {
-    ColumnList
+    ColumnList,
+    GlobalHeader
   },
   setup() {
     return {
-      testData
+      testData,
+      currentUser
     };
   }
 });

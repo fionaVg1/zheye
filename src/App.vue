cce2646c1153dc0388eb792147b58f7ec60fcb24
caiwanxia <caiwanxia@dm-ai.cn>
2021-03-05 21:13:32 +0800

    带验证功能的输入框组件

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,7 @@
     <form action="">
       <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
-        <input
-          type="email"
-          class="form-control"
-          id="exampleInputEmail1"
-          v-model="emailRef.val"
-          @blur="validateEmail"
-        />
+        <validate-input :rules="emailRules"></validate-input>
         <div class="form-text" v-if="emailRef.error">
           {{ emailRef.message }}
         </div>
@@ -31,6 +25,7 @@
 <script lang="ts">
 import { defineComponent, reactive } from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ValidateInput, { RulesProp } from "./components/ValidateInput.vue";
 import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
 import GlobalHeader, { UserProps } from "./components/GlobalHeader.vue";
 const currentUser: UserProps = {
@@ -61,9 +56,14 @@ export default defineComponent({
   name: "App",
   components: {
     ColumnList,
-    GlobalHeader
+    GlobalHeader,
+    ValidateInput
   },
   setup() {
+    const emailRules: RulesProp = [
+      { type: "required", message: "电子邮箱地址不能为空" },
+      { type: "email", message: "请输入正确的电子邮箱格式" }
+    ];
     const emailRef = reactive({
       val: "",
       error: false,
@@ -82,7 +82,8 @@ export default defineComponent({
       testData,
       currentUser,
       emailRef,
-      validateEmail
+      validateEmail,
+      emailRules
     };
   }
 });
